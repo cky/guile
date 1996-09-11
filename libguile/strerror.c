dffa93b1b2c5ba0826820d5dd720c46815f3adbb
Jim Blandy <jimb@red-bean.com>
1996-09-11 22:55:24 +0000

    Doc fix.

--- libguile/strerror.c
+++ libguile/strerror.c
@@ -1,4 +1,4 @@
-/* Interfaces to system-dependent kernel and library entries.
+/* Turning errno values into English error messages.
    Copyright (C) 1985, 86, 87, 88, 93, 94, 95 Free Software Foundation, Inc.
 
 This file is part of GNU Emacs.
