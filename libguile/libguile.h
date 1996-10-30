fd7932d3a6e8f7fd8c930e2c94194b44bb1f8efa
Mikael Djurfeldt <djurfeldt@nada.kth.se>
1996-10-30 23:36:51 +0000

    * libguile.h: #include "libguile/libpath.h"

--- libguile/libguile.h
+++ libguile/libguile.h
@@ -81,6 +81,7 @@
 #include "libguile/init.h"
 #include "libguile/ioext.h"
 #include "libguile/kw.h"
+#include "libguile/libpath.h"
 #include "libguile/list.h"
 #include "libguile/load.h"
 #include "libguile/mallocs.h"
