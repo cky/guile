9946dd45a43e19a0de11ebc07e4dc9ed727fdd1b
Jim Blandy <jimb@red-bean.com>
1997-06-22 23:46:41 +0000

    Try to detect when people are using one version of libguile and a different version of ice-9.  People have been skewing things and sending in bug reports. * version.scm.in: New file, which the configure script munges to produce version.scm, which contains the ice-9 config stamp. * boot-9.scm: Compare the libguile and ice-9 config stamps; display a warning if the two are different. * Makefile.am: Install version.scm, but don't distribute it. Distribute version.scm.in, but don't install it. * Makefile.in: Regenerated.

--- ice-9/boot-9.scm
+++ ice-9/boot-9.scm
@@ -3853,6 +3853,29 @@
     (define-module (guile) :use-module (ice-9 regex)))
 
 
+;;; {Check that the interpreter and scheme code match up.}
+
+(let ((show-line
+       (lambda args
+	 (with-output-to-port (current-error-port)
+	   (lambda ()
+	     (display (car (command-line)))
+	     (display ": ")
+	     (for-each (lambda (string) (display string))
+		       args) 
+	     (newline))))))
+
+  (load-from-path "ice-9/version.scm")
+
+  (if (not (string=?
+	    (libguile-config-stamp)	; from the interprpreter
+	    (ice-9-config-stamp)))	; from the Scheme code
+      (begin
+	(show-line "warning: different versions of libguile and ice-9:")
+	(show-line "libguile: configured on " (libguile-config-stamp))
+	(show-line "ice-9:    configured on " (ice-9-config-stamp)))))
+    
+
 
 (define-module (guile))
 
