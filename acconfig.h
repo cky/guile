cc77007c2b7f8b57bc60501cfa304f983422cff8
Jim Blandy <jimb@red-bean.com>
1998-10-19 15:28:47 +0000

    *** empty log message ***

--- acconfig.h
+++ acconfig.h
@@ -1,5 +1,4 @@
 /* acconfig.h --- documentation for symbols possibly defined in scmconfig.h
- * written by Jim Blandy <jimb@cyclic.com> --- August 1996
  * The `autoheader' command, from the autoconf suite, generates
  * libguile/scmconfig.h, based on configure.in and this file.
  *
