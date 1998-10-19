6f7e3d59151d87bd43ce27b2187db27de1fc5914
Jim Blandy <jimb@red-bean.com>
1998-10-19 21:55:35 +0000

    * image-type.c, myguile.c: Terminate copyright comments.

--- doc/example-smob/image-type.c
+++ doc/example-smob/image-type.c
@@ -16,6 +16,7 @@
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
  * Boston, MA 02111-1307 USA
+ */
 
 #include <stdlib.h>
 #include <libguile.h>
