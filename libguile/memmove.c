be881b4688376f98b5eaa5c0bfb2cded1cde06b6
Jim Blandy <jimb@red-bean.com>
1999-09-01 02:52:27 +0000

    I take it all back --- bcopy does handle overlapping source and destination areas correctly.  At least on every system I could find.  But it is better to use AC_REPLACE_FUNCS than to introduce new CPP conditionals. * memmove.c: New file, implementing memmove in terms of bcopy. * scmconfig.h.in: Regenerated.

---
+++ libguile/memmove.c
@@ -0,0 +1,24 @@
+/* Wrapper to implement ANSI C's memmove using BSD's bcopy. */
+/* This function is in the public domain.  --Per Bothner. */
+
+#include <sys/types.h>
+
+#ifdef __STDC__
+#define PTR void *
+#define CPTR const void *
+PTR memmove (PTR, CPTR, size_t);
+#else
+#define PTR char *
+#define CPTR char *
+PTR memmove ();
+#endif
+
+PTR
+memmove (s1, s2, n)
+     PTR s1;
+     CPTR s2;
+     size_t n;
+{
+  bcopy (s2, s1, n);
+  return s1;
+}
