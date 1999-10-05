0b49b6aea0c4e85a830006210bea074ba09d7b8e
Jim Blandy <jimb@red-bean.com>
1999-10-05 23:07:04 +0000

    * autogen.sh: Don't call autoreconf at all; it's not reliable. Instead, call the various tools explicitly.  Invoke guile-readline's autogen.sh script.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+[ -f GUILE-VERSION ] || {
+  echo "autogen.sh: run this command only at the top of a Guile source tree."
+  exit 1
+}
+
+aclocal -I .
 autoheader
-autoreconf --force "$@"
+autoconf
+automake
+
+( cd guile-readline; ./autogen.sh )
