665aeda3e51dd101f5f1e22c87218872c5b0a052
Dirk Herrmann <dirk@dirk-herrmanns-seiten.de>
2000-05-08 11:53:01 +0000

    Use SCM{_SET}?_CELL_OBJECT if a cell is not known to be a valid pair yet.

--- libguile/alist.c
+++ libguile/alist.c
@@ -65,12 +65,12 @@ SCM_DEFINE (scm_acons, "acons", 3, 0, 0,
   SCM head;
 
   SCM_NEWCELL (pair);
-  SCM_SETCAR (pair, key);
-  SCM_SETCDR (pair, value);
+  SCM_SET_CELL_OBJECT_0 (pair, key);
+  SCM_SET_CELL_OBJECT_1 (pair, value);
 
   SCM_NEWCELL (head);
-  SCM_SETCAR (head, pair);
-  SCM_SETCDR (head, alist);
+  SCM_SET_CELL_OBJECT_0 (head, pair);
+  SCM_SET_CELL_OBJECT_1 (head, alist);
 
   return head;
 }
