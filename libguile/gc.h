35e9b48d791e82b18d6ddb2ee244ddee841fc370
Michael Livshin <mlivshin@bigfoot.com>
2000-08-18 22:33:37 +0000

    * gc.h: removed some stuff that broke compilation for people and wasn't actually needed anyway.

--- libguile/gc.h
+++ libguile/gc.h
@@ -81,45 +81,6 @@ typedef scm_cell * SCM_CELLPTR;
 #  define PTR2SCM(x) (SCM_PACK ((scm_bits_t) (x)))
 #endif /* def _UNICOS */
 
-/* This mess was copied from the GNU getpagesize.h.  */
-
-#ifndef HAVE_GETPAGESIZE
-
-/* Assume that all systems that can run configure have sys/param.h.  */
-# ifndef HAVE_SYS_PARAM_H
-#  define HAVE_SYS_PARAM_H 1
-# endif
-
-# ifdef _SC_PAGESIZE
-#  define getpagesize() sysconf(_SC_PAGESIZE)
-# else /* no _SC_PAGESIZE */
-#  ifdef HAVE_SYS_PARAM_H
-#   include <sys/param.h>
-#   ifdef EXEC_PAGESIZE
-#    define getpagesize() EXEC_PAGESIZE
-#   else /* no EXEC_PAGESIZE */
-#    ifdef NBPG
-#     define getpagesize() NBPG * CLSIZE
-#     ifndef CLSIZE
-#      define CLSIZE 1
-#     endif /* no CLSIZE */
-#    else /* no NBPG */
-#     ifdef NBPC
-#      define getpagesize() NBPC
-#     else /* no NBPC */
-#      ifdef PAGESIZE
-#       define getpagesize() PAGESIZE
-#      endif /* PAGESIZE */
-#     endif /* no NBPC */
-#    endif /* no NBPG */
-#   endif /* no EXEC_PAGESIZE */
-#  else /* no HAVE_SYS_PARAM_H */
-#   define getpagesize() 8192	/* punt totally */
-#  endif /* no HAVE_SYS_PARAM_H */
-# endif /* no _SC_PAGESIZE */
-
-#endif /* no HAVE_GETPAGESIZE */
-
 #define SCM_GC_CARD_N_HEADER_CELLS 1
 #define SCM_GC_CARD_N_CELLS        (8 * sizeof (scm_cell) * 4)
 
