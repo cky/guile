b299f5cd5bb8c7324a50f3b1dfc21c3a0582f116
Mikael Djurfeldt <djurfeldt@nada.kth.se>
2000-08-27 03:21:03 +0000

    * stacks.c (scm_init_stacks): Pass a string, not a layout object, to scm_make_vtable_vtable.  (Thanks to Dale P. Smith.)

--- libguile/stacks.c
+++ libguile/stacks.c
@@ -734,10 +734,9 @@ void
 scm_init_stacks ()
 {
   SCM vtable;
-  SCM vtable_layout = scm_make_struct_layout (scm_nullstr);
   SCM stack_layout
     = scm_make_struct_layout (scm_makfrom0str (SCM_STACK_LAYOUT));
-  vtable = scm_make_vtable_vtable (vtable_layout, SCM_INUM0, SCM_EOL);
+  vtable = scm_make_vtable_vtable (scm_nullstr, SCM_INUM0, SCM_EOL);
   scm_stack_type
     = scm_permanent_object (scm_make_struct (vtable, SCM_INUM0,
 					     scm_cons (stack_layout,
