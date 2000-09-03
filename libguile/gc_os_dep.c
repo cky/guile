9500b5b95085cb7813c24e8bd5339edcb449b778
Marius Vollmer <mvo@zagadka.de>
2000-09-03 20:27:17 +0000

    New file.

---
+++ libguile/gc_os_dep.c
@@ -0,0 +1,10 @@
+/* This is a dummy file.  It will be replaced with the real thing when
+   all copyright issues have been settled. */
+
+#include <stddef.h>
+
+void *
+scm_get_stack_base ()
+{
+    return NULL;
+}
