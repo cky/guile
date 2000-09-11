a4908d56f4b3d80da1671603b34ec5e73f02aa8d
Keisuke Nishida <kxn30@po.cwru.edu>
2000-09-11 09:18:49 +0000

    * autogen.sh: Run aclocal with check where guile.m4 is installed.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-aclocal
+if test -f "`aclocal --print-ac-dir`/guile.m4"; then
+  aclocal
+else
+  if test -f "`guile-config info datadir`/aclocal/guile.m4"; then
+    aclocal -I "`guile-config info datadir`/aclocal"
+  else
+    echo "Cannot find guile.m4";
+    exit;
+  fi
+fi
+
 autoheader
 automake -a
 autoconf
