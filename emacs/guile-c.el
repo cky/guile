e41561b49031c43dcfeecc93130f9a357857acef
Keisuke Nishida <kxn30@po.cwru.edu>
2001-03-12 02:46:13 +0000

    *** empty log message ***

--- emacs/guile-c.el
+++ emacs/guile-c.el
@@ -1,6 +1,6 @@
 ;;; guile-c.el --- Guile C editing commands
 
-;; Copyright (C) 2001 Keisuke Nishida <kxn30@po.cwru.edu>
+;; Copyright (C) 2001 Free Software Foundation, Inc.
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
