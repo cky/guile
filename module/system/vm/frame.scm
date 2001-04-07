540c7f72a398ded0cf7cbf3f4cdb410d8f0b0c80
Keisuke Nishida <kxn30@po.cwru.edu>
2001-04-07 12:51:36 +0000

    *** empty log message ***

--- module/system/vm/frame.scm
+++ module/system/vm/frame.scm
@@ -27,14 +27,10 @@
   (let* ((prog (frame-program frame))
 	 (nargs (car (program-arity prog))))
     (do ((i 0 (1+ i))
-	 (l (reverse! (vector->list (frame-variables frame))) (cdr l))
+	 (l (vector->list (frame-variables frame)) (cdr l))
 	 (r '() (cons (car l) r)))
 	((= i nargs) (cons (program-name prog) r)))))
 
 (define (program-name x)
   (hash-fold (lambda (s v d) (if (eq? x (variable-ref v)) s d)) x
 	     (module-obarray (current-module))))
-
-; (define-method (binding (prog <program>))
-;   (fold (lambda (s v d) (if (eq? v prog) s d))
-; 	#f (program-environment prog)))
