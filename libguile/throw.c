1f7f9ed41d855099432fa6338d8e6a535dac41b3
Neil Jerram <neil@ossau.uklinux.net>
2001-04-22 22:16:07 +0000

    * Correct docstring for `throw'.

--- libguile/throw.c
+++ libguile/throw.c
@@ -592,7 +592,7 @@ SCM_DEFINE (scm_throw, "throw", 1, 0, 1,
 	    "@var{handler}.  \n\n"
 	    "@var{key} is a symbol.  It will match catches of the same symbol or of\n"
 	    "#t.\n\n"
-	    "If there is no handler at all, an error is signaled.")
+	    "If there is no handler at all, Guile prints an error and then exits.")
 #define FUNC_NAME s_scm_throw
 {
   SCM_VALIDATE_SYMBOL (1,key);
