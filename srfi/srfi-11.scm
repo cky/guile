46a7b46f0cfe6c477c3a23f271e31665736f6da4
Martin Grabmüller <mgrabmue@cs.tu-berlin.de>
2001-07-19 20:28:33 +0000

    * srfi-11.scm, srfi-8.scm: Update copyright notice.

--- srfi/srfi-11.scm
+++ srfi/srfi-11.scm
@@ -1,6 +1,6 @@
 ;;;; srfi-11.scm --- SRFI-11 procedures for Guile
 
-;;; Copyright (C) 2000 Free Software Foundation, Inc.
+;;; Copyright (C) 2000, 2001 Free Software Foundation, Inc.
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License as
