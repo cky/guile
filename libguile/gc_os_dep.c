f28108ee142c8c05c4273342a051e3018de22343
Rob Browning <rlb@defaultvalue.org>
2001-07-19 17:43:34 +0000

    * gc_os_dep.c (GC_noop1): ifdef out (unused) to quiet warning.

--- libguile/gc_os_dep.c
+++ libguile/gc_os_dep.c
@@ -62,14 +62,19 @@ typedef int GC_bool;
 #   define VOLATILE
 #endif
 
+#if 0 /* currently unused (as of 2001-07-12) */
+
 /* Single argument version, robust against whole program analysis. */
-static void GC_noop1(x)
+static void
+GC_noop1(x)
 word x;
 {
     static VOLATILE word sink;
 
     sink = x;
 }
+
+#endif
 
 /* Machine dependent parameters.  Some tuning parameters can be found	*/
 /* near the top of gc_private.h.					*/
