4cf7528804e08fa92c05c0c5ac5ae2eeaed0c48e
Thien-Thi Nguyen <ttn@gnuvola.org>
2001-08-24 22:00:45 +0000

    (string-for-each-index): New exported proc.

--- srfi/srfi-13.scm
+++ srfi/srfi-13.scm
@@ -1,17 +1,17 @@
 ;;;; srfi-13.scm --- SRFI-13 procedures for Guile
 ;;;;
 ;;;; 	Copyright (C) 2001 Free Software Foundation, Inc.
-;;;; 
+;;;;
 ;;;; This program is free software; you can redistribute it and/or
 ;;;; modify it under the terms of the GNU General Public License as
 ;;;; published by the Free Software Foundation; either version 2, or
 ;;;; (at your option) any later version.
-;;;; 
+;;;;
 ;;;; This program is distributed in the hope that it will be useful,
 ;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;;;; General Public License for more details.
-;;;; 
+;;;;
 ;;;; You should have received a copy of the GNU General Public License
 ;;;; along with this software; see the file COPYING.  If not, write to
 ;;;; the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
@@ -56,7 +56,7 @@
  (define string-downcase #f)
  (define string-downcase! #f))
 
-(export 
+(export
 ;;; Predicates
  ;; string? string-null?       <= in the core
  string-any string-every
@@ -134,6 +134,7 @@
  string-unfold
  string-unfold-right
  string-for-each
+ string-for-each-index
 
 ;;; Replicate/Rotate
  xsubstring string-xcopy!
