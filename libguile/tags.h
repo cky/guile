08ae8768723322b57147baaf01e7a37796b15c53
Marius Vollmer <mvo@zagadka.de>
2001-11-12 01:13:00 +0000

    (SIZEOF_SCM_T_BITS): Define it appropriately.

--- libguile/tags.h
+++ libguile/tags.h
@@ -65,12 +65,14 @@
 #ifdef HAVE_UINTPTR_T
 typedef uintptr_t scm_t_bits;
 typedef intptr_t scm_t_signed_bits;
+#define SIZEOF_SCM_T_BITS SIZEOF_UINTPTR_T
 #define SCM_T_BITS_MAX UINTPTR_MAX
 #define SCM_T_SIGNED_BITS_MAX INTPTR_MAX
 #define SCM_T_SIGNED_BITS_MIN INTPTR_MIN
 #else
 typedef unsigned long scm_t_bits;
 typedef signed long scm_t_signed_bits;
+#define SIZEOF_SCM_T_BITS SIZEOF_LONG
 #define SCM_T_BITS_MAX ULONG_MAX
 #define SCM_T_SIGNED_BITS_MAX LONG_MAX
 #define SCM_T_SIGNED_BITS_MIN LONG_MIN
