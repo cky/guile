c0fa6561ac9cd9c58b2329237c962d34e4403279
Thien-Thi Nguyen <ttn@gnuvola.org>
2002-02-09 00:38:43 +0000

    (create_gsubr): On "too many args" error, also display arg count and name.

--- libguile/gsubr.c
+++ libguile/gsubr.c
@@ -1,15 +1,15 @@
 /* Copyright (C) 1995,1996,1997,1998,1999,2000,2001 Free Software Foundation, Inc.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
@@ -62,7 +62,7 @@ SCM_GLOBAL_SYMBOL (scm_sym_name, "name");
 SCM scm_f_gsubr_apply;
 
 static SCM
-create_gsubr (int define, const char *name, 
+create_gsubr (int define, const char *name,
 	      int req, int opt, int rst, SCM (*fcn)())
 {
   SCM subr;
@@ -103,7 +103,9 @@ create_gsubr (int define, const char *name,
 	SCM sym = SCM_SUBR_ENTRY(subr).name;
 	if (SCM_GSUBR_MAX < req + opt + rst)
 	  {
-	    fputs ("ERROR in scm_c_make_gsubr: too many args\n", stderr);
+            fprintf (stderr,
+                     "ERROR in scm_c_make_gsubr: too many args (%d) to %s\n",
+                     req + opt + rst, name);
 	    exit (1);
 	  }
 	SCM_SET_GSUBR_PROC (cclo, subr);
@@ -215,8 +217,8 @@ scm_gsubr_apply (SCM args)
   long i, n = SCM_GSUBR_REQ (typ) + SCM_GSUBR_OPT (typ) + SCM_GSUBR_REST (typ);
 #if 0
   if (n > SCM_GSUBR_MAX)
-    scm_misc_error (FUNC_NAME, 
-		    "Function ~S has illegal arity ~S.", 
+    scm_misc_error (FUNC_NAME,
+		    "Function ~S has illegal arity ~S.",
 		    scm_list_2 (self, SCM_MAKINUM (n)));
 #endif
   args = SCM_CDR (args);
@@ -258,7 +260,7 @@ scm_gsubr_apply (SCM args)
 
 #ifdef GSUBR_TEST
 /* A silly example, taking 2 required args, 1 optional, and
-   a scm_list of rest args 
+   a scm_list of rest args
    */
 SCM
 gsubr_21l(SCM req1, SCM req2, SCM opt, SCM rst)
