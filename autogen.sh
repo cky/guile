fc4fc6f6f0ed6e9e88ea02032ca0525191f0d6c1
Rob Browning <rlb@defaultvalue.org>
2002-02-25 04:46:41 +0000

    * autogen.sh: make absolutely sure we can't have stale files from old versions lying around the libltdl dir since libtoolize doesn't.  Also hack libltdl's configure.in to require autoconf 2.5 so the main tree and libltdl can't get out of sync again.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 [ -f GUILE-VERSION ] || {
   echo "autogen.sh: run this command only at the top of a Guile source tree."
   exit 1
@@ -7,16 +9,32 @@
 
 ./guile-aclocal.sh
 
-libtoolize --copy --force --automake --ltdl
+######################################################################
+### Libtool setup.
+
+# Get a clean version.
+rm -rf libltdl
+libtoolize --force --copy --automake --ltdl
+
+# Make sure we use a ./configure.in compatible autoconf in ./libltdl/
+mv libltdl/configure.in libltdl/configure.tmp
+echo 'AC_PREREQ(2.50)' > libltdl/configure.in
+cat libltdl/configure.tmp >> libltdl/configure.in
+rm libltdl/configure.tmp
+######################################################################
+
 autoheader
 autoconf
 automake --add-missing
 
 # Make sure that libltdl uses the same autoconf version as the rest.
 #
-( echo "libltdl..."; cd libltdl; autoconf )
+echo "libltdl..."
+(cd libltdl && autoconf)
+(cd libltdl && automake --gnu --add-missing)
 
-( echo "guile-readline..."; cd guile-readline; ./autogen.sh )
+echo "guile-readline..."
+(cd guile-readline && ./autogen.sh)
 
 echo "Now run configure and make."
 echo "You must pass the \`--enable-maintainer-mode' option to configure."
