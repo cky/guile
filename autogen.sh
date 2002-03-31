3be5cdf247470688e44391b8a91d608c47cf1d2c
Thien-Thi Nguyen <ttn@gnuvola.org>
2002-03-31 23:18:06 +0000

    Add usage comment. Add workbook specification. Add dist-files symlinking.

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,34 @@
 #!/bin/sh
+# Usage: sh -x ./autogen.sh [WORKBOOK]
 
 set -e
 
 [ -f GUILE-VERSION ] || {
-  echo "autogen.sh: run this command only at the top of a Guile source tree."
+  echo "autogen.sh: run this command only at the top of guile-core."
   exit 1
 }
+
+######################################################################
+### Find workbook and make symlinks.
+
+workbook=../workbook                    # assume "cvs co hack"
+test x$1 = x || workbook=$1
+if [ ! -d $workbook ] ; then
+    echo "ERROR: could not find workbook dir"
+    echo "       re-run like so: $0 WORKBOOK"
+    exit 1
+fi
+: found workbook at $workbook
+workbook=`(cd $workbook ; pwd)`
+
+workbookdistfiles="ANON-CVS HACKING INSTALL SNAPSHOTS"
+for f in $workbookdistfiles ; do
+    rm -f $f
+    ln -s $workbook/build/dist-files/$f $f
+done
+rm -f examples/example.gdbinit
+ln -s $workbook/build/dist-files/.gdbinit examples/example.gdbinit
+######################################################################
 
 ./guile-aclocal.sh
 
@@ -38,3 +61,5 @@ echo "guile-readline..."
 
 echo "Now run configure and make."
 echo "You must pass the \`--enable-maintainer-mode' option to configure."
+
+# autogen.sh ends here
