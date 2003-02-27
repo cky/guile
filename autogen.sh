549d7a55a008b2455ba65f090d94ee58317dd933
Rob Browning <rlb@defaultvalue.org>
2003-02-27 07:08:28 +0000

    * autogen.sh: switch to autoreconf -- see how it goes.  remove call to guile-aclocal.sh -- we now do the same thing with an automake setting.

--- autogen.sh
+++ autogen.sh
@@ -35,75 +35,12 @@ rm -f BUGS
 $mscripts/render-bugs > BUGS
 
 ######################################################################
+### update infrastructure
 
-# Make sure this matches the ACLOCAL invokation in Makefile.am
-
-./guile-aclocal.sh
-
-######################################################################
-### Libtool setup.
-
-# Get a clean version.
-libtoolize --force --copy --automake
-
-######################################################################
-
-ac_version=`autoconf --version | head -1`
-ac_version=`echo ${ac_version} | sed -e 's/autoconf.* \([0-9].[0-9]\+\)$/\1/'`
-case "${ac_version}" in
-  (2.5?) autoconf=autoconf; autoheader=autoheader ;;
-esac
-
-# configure.in reqs autoconf-2.53; try to find it
-if test -z "$autoconf"; then
-  for suf in "-2.53" "2.53" ""  false; do
-    version=`autoconf$suf --version 2>/dev/null | head -1 | awk '{print $NF}' | awk -F. '{print $1 * 100 + $2}'`
-    if test "0$version" -eq 253; then
-      autoconf=autoconf$suf
-      autoheader=autoheader$suf
-      break
-    fi
-  done
-fi
-
-if test -z "$autoconf"; then
-    echo "ERROR: Please install autoconf 2.53"
-    exit 1
-fi
-################################################################
-
-#detect automake version
-
-
-# configure.in requires particular automake; try to find it
-for suf in "-1.7" "1.7" "-1.6" "1.6" ""  false; do
-  version=`automake$suf --version 2>/dev/null | head -1 | awk '{print $NF}' | awk -F. '{print $1 * 10 + $2}'`
-  if test "0$version" -eq 17 -o "0$version" -eq 16; then
-    automake=automake$suf
-    break
-  fi
-done
-
-if test -z "$automake"; then
-    echo "ERROR: Please install automake 1.6.x"
-    exit 1
-fi
-
-################################################################
-$autoheader
-$autoconf
-
-# Automake has a bug that will let it only add one copy of a missing
-# file.  We need two mdate-sh, tho, one in doc/ref/ and one in
-# doc/tutorial/.  We run automake twice as a workaround.
-
-$automake --add-missing
-$automake --add-missing
+autoreconf -i
 
 echo "guile-readline..."
 (cd guile-readline && ./autogen.sh)
 
 echo "Now run configure and make."
 echo "You must pass the \`--enable-maintainer-mode' option to configure."
-
-# autogen.sh ends here
