6ada43c9441f841f76f302a0e9a0ab8b75383adb
Rob Browning <rlb@defaultvalue.org>
2003-03-26 00:00:30 +0000

    * strop.c: #include <config.h> if HAVE_CONFIG_H.

--- libguile/strop.c
+++ libguile/strop.c
@@ -41,9 +41,10 @@ If you write modifications of your own for GUILE, it is your choice
 whether to permit this exception to apply to your modifications.
 If you do not wish that, delete this exception notice.  */
 
-
-
 
+#if HAVE_CONFIG_H
+#  include <config.h>
+#endif
 
 #include <errno.h>
 
