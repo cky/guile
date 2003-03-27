9e443ab676a52a7e7c88ae7559774b9dd7d2103d
Rob Browning <rlb@defaultvalue.org>
2003-03-27 20:07:10 +0000

    * evalext.c: fix various preprocessor usages of new public symbols to expect 0 or 1 values rather than 1 or undefined. i.e. change #ifdef to #if, etc.

--- libguile/evalext.c
+++ libguile/evalext.c
@@ -166,7 +166,7 @@ SCM_DEFINE (scm_self_evaluating_p, "self-evaluating?", 1, 0, 0,
 	case scm_tcs_closures:
 	case scm_tc7_vector:
 	case scm_tc7_wvect:
-#ifdef SCM_HAVE_ARRAYS
+#if SCM_HAVE_ARRAYS
 	case scm_tc7_bvect:
 	case scm_tc7_byvect:
 	case scm_tc7_svect:
