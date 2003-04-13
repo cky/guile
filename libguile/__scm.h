7c12f0ac3de32d0c560f3ebcaef62a781980e417
Rob Browning <rlb@defaultvalue.org>
2003-04-13 17:46:09 +0000

    * __scm.h (SCM_FLUSH_REGISTER_WINDOWS): don't just rely on "sparc" test.  Instead use #if defined (sparc) || defined (__sparc__) || defined (__sparc) as gc_os_dep.c suggests is appropriate.

--- libguile/__scm.h
+++ libguile/__scm.h
@@ -337,7 +337,7 @@
  * that all the state of the process is contained in the stack.
  */
 
-#ifdef sparc
+#if defined (sparc) || defined (__sparc__) || defined (__sparc)
 # define SCM_FLUSH_REGISTER_WINDOWS asm("ta 3")
 #else
 # define SCM_FLUSH_REGISTER_WINDOWS /* empty */
