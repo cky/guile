7aaf8dc9f768bce59b295fc09df50ed5cd7efdf1
Mikael Djurfeldt <djurfeldt@nada.kth.se>
2003-05-12 16:29:45 +0000

    (SCM_GC8MARKP, SCM_SETGC8MARK, SCM_CLRGC8MARK): Use current names in definitions.

--- libguile/deprecated.h
+++ libguile/deprecated.h
@@ -58,9 +58,9 @@ SCM_API SCM scm_unprotect_object (SCM obj);
   (SCM_SETCDR ((x), SCM_PACK (SCM_UNPACK (SCM_CDR (x)) | (y))))
 #define SCM_FREEP(x) (SCM_FREE_CELL_P (x))
 #define SCM_NFREEP(x) (!SCM_FREE_CELL_P (x))
-#define SCM_GC8MARKP(x) SCM_GCMARKP (x)
-#define SCM_SETGC8MARK(x) SCM_SETGCMARK (x)
-#define SCM_CLRGC8MARK(x) SCM_CLRGCMARK (x)
+#define SCM_GC8MARKP(x) SCM_GC_MARK_P (x)
+#define SCM_SETGC8MARK(x) SCM_SET_GC_MARK (x)
+#define SCM_CLRGC8MARK(x) SCM_CLEAR_GC_MARK (x)
 #define SCM_GCTYP16(x) SCM_TYP16 (x)
 #define SCM_GCCDR(x) SCM_CDR (x)
 SCM_API void scm_remember (SCM * ptr);
