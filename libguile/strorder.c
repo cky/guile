2c0b7c1fa0dc4760418133b9eeebc0561a2dd4e0
Marius Vollmer <mvo@zagadka.de>
2004-08-24 22:13:14 +0000

    (scm_string_equal_p, scm_string_ci_equal_p, scm_string_less_p, scm_string_leq_p, scm_string_gr_p, scm_string_geq_p, scm_string_ci_less_p, scm_string_ci_leq_p, scm_string_ci_gr_p, scm_string_ci_geq_p): Use scm_string_eq, etc instead of explicit code.

--- libguile/strorder.c
+++ libguile/strorder.c
@@ -1,4 +1,4 @@
-/*	Copyright (C) 1995, 1996, 1999, 2000 Free Software Foundation, Inc.
+/*	Copyright (C) 1995, 1996, 1999, 2000, 2004 Free Software Foundation, Inc.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -24,7 +24,19 @@
 
 #include "libguile/validate.h"
 #include "libguile/strorder.h"
+#include "libguile/srfi-13.h"
 
+
+SCM_C_INLINE_KEYWORD static SCM
+srfi13_cmp (SCM s1, SCM s2, SCM (*cmp) (SCM, SCM, SCM, SCM, SCM, SCM))
+{
+  if (scm_is_true (cmp (s1, s2,
+			SCM_UNDEFINED, SCM_UNDEFINED,
+			SCM_UNDEFINED, SCM_UNDEFINED)))
+    return SCM_BOOL_T;
+  else
+    return SCM_BOOL_F;
+}
 
 SCM_DEFINE1 (scm_string_equal_p, "string=?", scm_tc7_rpsubr,
              (SCM s1, SCM s2),
@@ -38,36 +50,9 @@ SCM_DEFINE1 (scm_string_equal_p, "string=?", scm_tc7_rpsubr,
 	    "characters.")
 #define FUNC_NAME s_scm_string_equal_p
 {
-  size_t length;
-
-  SCM_VALIDATE_STRING (1, s1);
-  SCM_VALIDATE_STRING (2, s2);
-
-  length = scm_i_string_length (s2);
-  if (scm_i_string_length (s1) == length)
-    {
-      const unsigned char *c1 = scm_i_string_chars (s1) + length - 1;
-      const unsigned char *c2 = scm_i_string_chars (s2) + length - 1;
-      size_t i;
-
-      /* comparing from back to front typically finds mismatches faster */
-      for (i = 0; i != length; ++i, --c1, --c2)
-	if (*c1 != *c2)
-	  {
-	    scm_remember_upto_here_2 (s1, s2);
-	    return SCM_BOOL_F;
-	  }
-
-      scm_remember_upto_here_2 (s1, s2);
-      return SCM_BOOL_T;
-    }
-  else
-    {
-      return SCM_BOOL_F;
-    }
+  return srfi13_cmp (s1, s2, scm_string_eq);
 }
 #undef FUNC_NAME
-
 
 SCM_DEFINE1 (scm_string_ci_equal_p, "string-ci=?", scm_tc7_rpsubr,
              (SCM s1, SCM s2),
@@ -77,62 +62,9 @@ SCM_DEFINE1 (scm_string_ci_equal_p, "string-ci=?", scm_tc7_rpsubr,
 	    "return @code{#f}.")
 #define FUNC_NAME s_scm_string_ci_equal_p
 {
-  size_t length;
-
-  SCM_VALIDATE_STRING (1, s1);
-  SCM_VALIDATE_STRING (2, s2);
-
-  length = scm_i_string_length (s2);
-  if (scm_i_string_length (s1) == length)
-    {
-      const unsigned char *c1 = scm_i_string_chars (s1) + length - 1;
-      const unsigned char *c2 = scm_i_string_chars (s2) + length - 1;
-      size_t i;
-
-      /* comparing from back to front typically finds mismatches faster */
-      for (i = 0; i != length; ++i, --c1, --c2)
-	if (scm_c_upcase (*c1) != scm_c_upcase (*c2))
-	  {
-	    scm_remember_upto_here_2 (s1, s2);
-	    return SCM_BOOL_F;
-	  }
-
-      scm_remember_upto_here_2 (s1, s2);
-      return SCM_BOOL_T;
-    }
-  else
-    {
-      return SCM_BOOL_F;
-    }
+  return srfi13_cmp (s1, s2, scm_string_ci_eq);
 }
 #undef FUNC_NAME
-
-
-/* Helper function for the lexicographic ordering predicates.
- * No argument checking is performed.  */
-static SCM
-string_less_p (SCM s1, SCM s2)
-{
-  size_t i, length1, length2, lengthm;
-  const unsigned char *c1, *c2;
-
-  length1 = scm_i_string_length (s1);
-  length2 = scm_i_string_length (s2);
-  lengthm = min (length1, length2);
-  c1 = scm_i_string_chars (s1);
-  c2 = scm_i_string_chars (s2);
-
-  for (i = 0; i != lengthm; ++i, ++c1, ++c2) {
-    int c = *c1 - *c2;
-    if (c == 0)
-      continue;
-    scm_remember_upto_here_2 (s1, s2);
-    return scm_from_bool (c < 0);
-  }
-
-  return scm_from_bool (length1 < length2);
-}
-
 
 SCM_DEFINE1 (scm_string_less_p, "string<?", scm_tc7_rpsubr,
              (SCM s1, SCM s2),
@@ -140,13 +72,9 @@ SCM_DEFINE1 (scm_string_less_p, "string<?", scm_tc7_rpsubr,
 	    "is lexicographically less than @var{s2}.")
 #define FUNC_NAME s_scm_string_less_p
 {
-  SCM_VALIDATE_STRING (1, s1);
-  SCM_VALIDATE_STRING (2, s2);
-
-  return string_less_p (s1, s2);
+  return srfi13_cmp (s1, s2, scm_string_lt);
 }
 #undef FUNC_NAME
-
 
 SCM_DEFINE1 (scm_string_leq_p, "string<=?", scm_tc7_rpsubr,
              (SCM s1, SCM s2),
@@ -154,13 +82,9 @@ SCM_DEFINE1 (scm_string_leq_p, "string<=?", scm_tc7_rpsubr,
 	    "is lexicographically less than or equal to @var{s2}.")
 #define FUNC_NAME s_scm_string_leq_p
 {
-  SCM_VALIDATE_STRING (1, s1);
-  SCM_VALIDATE_STRING (2, s2);
-
-  return scm_not (string_less_p (s2, s1));
+  return srfi13_cmp (s1, s2, scm_string_le);
 }
 #undef FUNC_NAME
-
 
 SCM_DEFINE1 (scm_string_gr_p, "string>?", scm_tc7_rpsubr,
              (SCM s1, SCM s2),
@@ -168,13 +92,9 @@ SCM_DEFINE1 (scm_string_gr_p, "string>?", scm_tc7_rpsubr,
 	    "is lexicographically greater than @var{s2}.")
 #define FUNC_NAME s_scm_string_gr_p
 {
-  SCM_VALIDATE_STRING (1, s1);
-  SCM_VALIDATE_STRING (2, s2);
-
-  return string_less_p (s2, s1);
+  return srfi13_cmp (s1, s2, scm_string_gt);
 }
 #undef FUNC_NAME
-
 
 SCM_DEFINE1 (scm_string_geq_p, "string>=?", scm_tc7_rpsubr,
              (SCM s1, SCM s2),
@@ -182,39 +102,9 @@ SCM_DEFINE1 (scm_string_geq_p, "string>=?", scm_tc7_rpsubr,
 	    "is lexicographically greater than or equal to @var{s2}.")
 #define FUNC_NAME s_scm_string_geq_p
 {
-  SCM_VALIDATE_STRING (1, s1);
-  SCM_VALIDATE_STRING (2, s2);
-
-  return scm_not (string_less_p (s1, s2));
+  return srfi13_cmp (s1, s2, scm_string_ge);
 }
 #undef FUNC_NAME
-
-
-/* Helper function for the case insensitive lexicographic ordering
- * predicates.  No argument checking is performed.  */
-static SCM
-string_ci_less_p (SCM s1, SCM s2)
-{
-  size_t i, length1, length2, lengthm;
-  const unsigned char *c1, *c2;
-
-  length1 = scm_i_string_length (s1);
-  length2 = scm_i_string_length (s2);
-  lengthm = min (length1, length2);
-  c1 = scm_i_string_chars (s1);
-  c2 = scm_i_string_chars (s2);
-
-  for (i = 0; i != lengthm; ++i, ++c1, ++c2) {
-    int c = scm_c_upcase (*c1) - scm_c_upcase (*c2);
-    if (c == 0)
-      continue;
-    scm_remember_upto_here_2 (s1, s2);
-    return scm_from_bool (c < 0);
-  }
-
-  return scm_from_bool (length1 < length2);
-}
-
 
 SCM_DEFINE1 (scm_string_ci_less_p, "string-ci<?", scm_tc7_rpsubr,
              (SCM s1, SCM s2),
@@ -223,13 +113,9 @@ SCM_DEFINE1 (scm_string_ci_less_p, "string-ci<?", scm_tc7_rpsubr,
 	    "regardless of case.")
 #define FUNC_NAME s_scm_string_ci_less_p
 {
-  SCM_VALIDATE_STRING (1, s1);
-  SCM_VALIDATE_STRING (2, s2);
-
-  return string_ci_less_p (s1, s2);
+  return srfi13_cmp (s1, s2, scm_string_ci_lt);
 }
 #undef FUNC_NAME
-
 
 SCM_DEFINE1 (scm_string_ci_leq_p, "string-ci<=?", scm_tc7_rpsubr,
              (SCM s1, SCM s2),
@@ -238,13 +124,9 @@ SCM_DEFINE1 (scm_string_ci_leq_p, "string-ci<=?", scm_tc7_rpsubr,
 	    "to @var{s2} regardless of case.")
 #define FUNC_NAME s_scm_string_ci_leq_p
 {
-  SCM_VALIDATE_STRING (1, s1);
-  SCM_VALIDATE_STRING (2, s2);
-
-  return scm_not (string_ci_less_p (s2, s1));
+  return srfi13_cmp (s1, s2, scm_string_ci_le);
 }
 #undef FUNC_NAME
-
 
 SCM_DEFINE1 (scm_string_ci_gr_p, "string-ci>?", scm_tc7_rpsubr,
              (SCM s1, SCM s2),
@@ -253,13 +135,9 @@ SCM_DEFINE1 (scm_string_ci_gr_p, "string-ci>?", scm_tc7_rpsubr,
 	    "@var{s2} regardless of case.")
 #define FUNC_NAME s_scm_string_ci_gr_p
 {
-  SCM_VALIDATE_STRING (1, s1);
-  SCM_VALIDATE_STRING (2, s2);
-
-  return string_ci_less_p (s2, s1);
+  return srfi13_cmp (s1, s2, scm_string_ci_gt);
 }
 #undef FUNC_NAME
-
 
 SCM_DEFINE1 (scm_string_ci_geq_p, "string-ci>=?", scm_tc7_rpsubr,
              (SCM s1, SCM s2),
@@ -268,10 +146,7 @@ SCM_DEFINE1 (scm_string_ci_geq_p, "string-ci>=?", scm_tc7_rpsubr,
 	    "equal to @var{s2} regardless of case.")
 #define FUNC_NAME s_scm_string_ci_geq_p
 {
-  SCM_VALIDATE_STRING (1, s1);
-  SCM_VALIDATE_STRING (2, s2);
-
-  return scm_not (string_ci_less_p (s1, s2));
+  return srfi13_cmp (s1, s2, scm_string_ci_ge);
 }
 #undef FUNC_NAME
 
