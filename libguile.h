f7e666545025dababb029a1dfcfd858a27d67805
Marius Vollmer <mvo@zagadka.de>
2004-09-24 02:05:18 +0000

    Include <gmp.h> outside of extern "C" block.  (Note that numbers.h still includes gmp.h to make it self-contained.)

--- libguile.h
+++ libguile.h
@@ -1,7 +1,7 @@
 #ifndef SCM_LIBGUILE_H
 #define SCM_LIBGUILE_H
 
-/* Copyright (C) 1995,1996,1997,1998,2000,2001, 2002, 2003 Free Software Foundation, Inc.
+/* Copyright (C) 1995,1996,1997,1998,2000,2001, 2002, 2003, 2004 Free Software Foundation, Inc.
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -42,6 +42,10 @@
  * whether to permit this exception to apply to your modifications.
  * If you do not wish that, delete this exception notice.  */
 
+
+/* This needs to be included outside of the extern "C" block.
+ */
+#include <gmp.h>
 
 #ifdef __cplusplus
 extern "C" {
