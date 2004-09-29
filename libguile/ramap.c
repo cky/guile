4f943a29c2da33d5e3e595e2cf78870c04b89523
Marius Vollmer <mvo@zagadka.de>
2004-09-29 15:48:21 +0000

    (scm_array_equal_p): Include scm_tc7_svect in switch.  Thanks to Roland Orre!

--- libguile/ramap.c
+++ libguile/ramap.c
@@ -1986,6 +1986,7 @@ scm_array_equal_p (SCM ra0, SCM ra1)
     case scm_tc7_byvect:
     case scm_tc7_uvect:
     case scm_tc7_ivect:
+    case scm_tc7_svect:
     case scm_tc7_fvect:
     case scm_tc7_dvect:
     case scm_tc7_cvect:
@@ -2005,6 +2006,7 @@ scm_array_equal_p (SCM ra0, SCM ra1)
     case scm_tc7_byvect:
     case scm_tc7_uvect:
     case scm_tc7_ivect:
+    case scm_tc7_svect:
     case scm_tc7_fvect:
     case scm_tc7_dvect:
     case scm_tc7_cvect:
