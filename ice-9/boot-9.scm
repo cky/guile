92b51af6c16ec2f71a9705678b8cc6e3d9b97008
Marius Vollmer <mvo@zagadka.de>
2004-10-04 17:57:39 +0000

    (symbol->keyword, keyword->symbol): Removed, they are now implemented in C.

--- ice-9/boot-9.scm
+++ ice-9/boot-9.scm
@@ -281,13 +281,6 @@
 ;;; {Keywords}
 ;;;
 
-(define (symbol->keyword symbol)
-  (make-keyword-from-dash-symbol (symbol-append '- symbol)))
-
-(define (keyword->symbol kw)
-  (let ((sym (symbol->string (keyword-dash-symbol kw))))
-    (string->symbol (substring sym 1 (string-length sym)))))
-
 (define (kw-arg-ref args kw)
   (let ((rem (member kw args)))
     (and rem (pair? (cdr rem)) (cadr rem))))
