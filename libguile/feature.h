de6eff7190b343e71baa282f0700dec60289ed8f
Kevin Ryde <user42@zip.com.au>
2007-01-15 23:42:46 +0000

    copyright year

--- libguile/feature.h
+++ libguile/feature.h
@@ -3,7 +3,7 @@
 #ifndef SCM_FEATURE_H
 #define SCM_FEATURE_H
 
-/* Copyright (C) 1995,1996,1999,2000,2001, 2006 Free Software Foundation, Inc.
+/* Copyright (C) 1995,1996,1999,2000,2001, 2006, 2007 Free Software Foundation, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
