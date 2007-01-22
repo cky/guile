cb90e2cbe7cc0ea11c4f3f7310ee9f07f59ecd4a
Han-Wen Nienhuys <hanwen@lilypond.org>
2007-01-22 15:11:30 +0000

    add FSF header.

--- libguile/private-gc.h
+++ libguile/private-gc.h
@@ -1,7 +1,22 @@
 /*
-  (c) FSF 2002. 
-*/
-   
+ * private-gc.h - private declarations for garbage collection.
+ * 
+ * Copyright (C) 2002, 03, 04, 05, 06, 07 Free Software Foundation, Inc.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 #ifndef PRIVATE_GC
 #define PRIVATE_GC
