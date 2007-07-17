df4497227344b13df10e2a4f7ae8811a5fbaca17
Ludovic Courtès <ludo@gnu.org>
2007-07-17 08:39:35 +0000

    Changes from arch/CVS synchronization

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Usage: sh -x ./autogen.sh [WORKBOOK]
+# Usage: sh -x ./autogen.sh
 
 set -e
 
@@ -7,34 +7,6 @@ set -e
   echo "autogen.sh: run this command only at the top of guile-core."
   exit 1
 }
-
-######################################################################
-### Find workbook and make symlinks.
-
-workbook=../workbook                    # assume "cvs co hack"
-test x$1 = x || workbook=$1
-if [ ! -d $workbook ] ; then
-    echo "ERROR: could not find workbook dir"
-    echo "       re-run like so: $0 WORKBOOK"
-    exit 1
-fi
-: found workbook at $workbook
-workbook=`(cd $workbook ; pwd)`
-
-workbookdistfiles="ANON-CVS HACKING SNAPSHOTS"
-for f in $workbookdistfiles ; do
-    rm -f $f
-    ln -s $workbook/build/dist-files/$f $f
-done
-rm -f examples/example.gdbinit
-ln -s $workbook/build/dist-files/.gdbinit examples/example.gdbinit
-
-# TODO: This should be moved to dist-guile
-mscripts=../guile-scripts
-if test -x $mscripts/render-bugs ; then
-  rm -f BUGS
-  $mscripts/render-bugs > BUGS
-fi
 
 ######################################################################
 ### announce build tool versions
