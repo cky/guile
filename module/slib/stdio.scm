83dff6e55f8397219e81b3f66d7d47c46c14b1cb
Andy Wingo <wingo@pobox.com>
2008-05-13 00:07:40 +0200

    Update Makefile.am's; remove slib import
    * Makefile.am:
    * module/Makefile.am:
    * module/language/scheme/Makefile.am:
    * module/system/Makefile.am:
    * module/system/base/Makefile.am:
    * module/system/il/Makefile.am:
    * module/system/repl/Makefile.am:
    * module/system/vm/Makefile.am: Cleaned up to be more complete, if not
      completely working.
    
    * module/guile/slib.scm:
    * module/slib/: Removed the slib import; it's a bit out of place here,
      and bitrotten at that.

--- module/slib/stdio.scm
+++
@@ -1,8 +0,0 @@
-;; "stdio.scm" compatability stub
-
-(require 'scanf)
-(require 'printf)
-
-(define stdin (current-input-port))
-(define stdout (current-output-port))
-(define stderr (current-error-port))
