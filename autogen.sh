292bb8fc681d7e9216809da233a32f4de6688adc
Andy Wingo <wingo@pobox.com>
2008-05-20 12:21:40 +0200

    include local copy of guile.m4
    * acinclude.m4: Add guile.m4 to this, because I really can't be arsed
      with aclocal.
    
    * autogen.sh: More better vanilla.

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-
-export ACLOCAL_AMFLAGS=
-if test ! -f "`aclocal --print-ac-dir`/guile.m4"; then
-  if test -f "`guile-config info datadir`/aclocal/guile.m4"; then
-    ACLOCAL_AMFLAGS="-I `guile-config info datadir`/aclocal"
-  else
-    echo "warning: cannot find guile.m4";
-  fi
-fi
-
 autoreconf -vif
