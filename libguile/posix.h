b6137ed741eb0f14a4fb68c98879b91ec8250039
Ludovic Courtès <ludo@gnu.org>
2008-07-04 22:22:06 +0200

    Don't declare `scm_i_locale_mutex' as `SCM_INTERNAL'.

--- libguile/posix.h
+++ libguile/posix.h
@@ -86,7 +86,7 @@ SCM_API SCM scm_sethostname (SCM name);
 SCM_API SCM scm_gethostname (void);
 SCM_INTERNAL void scm_init_posix (void);
 
-SCM_INTERNAL scm_i_pthread_mutex_t scm_i_locale_mutex;
+SCM_API scm_i_pthread_mutex_t scm_i_locale_mutex;
 
 #endif  /* SCM_POSIX_H */
 
