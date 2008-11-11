32c8ae2009b8ec3ec395a492dc1720ac5456343a
Andy Wingo <wingo@pobox.com>
2008-11-11 23:11:27 +0100

    rashly bump stack limit to 40k words
    * libguile/eval.c (scm_debug_opts): Whereas, today's machines are larger
      than yesterday's; GCC consumes more words per stack frame than it used
      to; and you can get quite some recursion in a halfway-compiled system,
      be it resolved: let's bump up the C stack limit to 40k words (160 kB /
      320 kB, depending on word size).

--- libguile/eval.c
+++ libguile/eval.c
@@ -2966,7 +2966,7 @@ scm_t_option scm_debug_opts[] = {
   { SCM_OPTION_BOOLEAN, "backtrace", 0, "Show backtrace on error." },
   { SCM_OPTION_BOOLEAN, "debug", 0, "Use the debugging evaluator." },
 
-  { SCM_OPTION_INTEGER, "stack", 20000, "Stack size limit (measured in words; 0 = no check)." },
+  { SCM_OPTION_INTEGER, "stack", 40000, "Stack size limit (measured in words; 0 = no check)." },
   { SCM_OPTION_SCM, "show-file-name", (unsigned long)SCM_BOOL_T,
     "Show file names and line numbers "
     "in backtraces when not `#f'.  A value of `base' "
