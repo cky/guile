68a30f5730e73b9792565c3c99b5724752472a85
Michael Gran <spk121@yahoo.com>
2009-08-20 21:31:58 -0700

    Type-limits error in GC environment initialization
    * libguile/gc-malloc.c (scm_gc_init_malloc): GUILE_INIT_MALLOC_LIMIT
      is cast to unsigned then tested as if it were still signed

--- libguile/gc-malloc.c
+++ libguile/gc-malloc.c
@@ -83,7 +83,7 @@ static int scm_i_minyield_malloc;
 void
 scm_gc_init_malloc (void)
 {
-  scm_mtrigger = scm_getenv_int ("GUILE_INIT_MALLOC_LIMIT",
+  int mtrigger = scm_getenv_int ("GUILE_INIT_MALLOC_LIMIT",
 				 SCM_DEFAULT_INIT_MALLOC_LIMIT);
   scm_i_minyield_malloc = scm_getenv_int ("GUILE_MIN_YIELD_MALLOC",
 					  SCM_DEFAULT_MALLOC_MINYIELD);
@@ -93,8 +93,10 @@ scm_gc_init_malloc (void)
   if (scm_i_minyield_malloc < 1)
     scm_i_minyield_malloc = 1;
 
-  if (scm_mtrigger < 0)
+  if (mtrigger < 0)
     scm_mtrigger = SCM_DEFAULT_INIT_MALLOC_LIMIT;
+  else
+    scm_mtrigger = mtrigger;
 }
 
 
