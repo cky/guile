cky/guile

commit e20d7001c3f7150400169fecb0bf0eefdf122fe2

Remove "compiled closures" ("cclos") in favor of a simpler mechanism.
The idea is to introduce `gsubrs' whose arity is encoded in their type
(more precisely in the sizeof (void *) - 8 MSBs).  This removes the
indirection introduced by cclos and simplifies the code.

* libguile/__scm.h (CCLO): Remove.

* libguile/debug.c (scm_procedure_source, scm_procedure_environment):
  Remove references to `scm_tc7_cclo'.

* libguile/eval.c (scm_trampoline_0, scm_trampoline_1,
  scm_trampoline_2): Replace `scm_tc7_cclo' with `scm_tc7_gsubr'.

* libguile/eval.i.c (CEVAL): Likewise.  No longer make PROC the first
  argument.  Directly invoke `scm_gsubr_apply ()' instead of jump to the
  `evap(N+1)' label or call to `SCM_APPLY ()'.

* libguile/evalext.c (scm_self_evaluating_p): Remove reference to
  `scm_tc7_cclo'.

* libguile/gc-card.c (scm_i_sweep_card, scm_i_tag_name): Likewise.

* libguile/gc-mark.c (scm_gc_mark_dependencies): Likewise.

* libguile/goops.c (scm_class_of): Likewise.

* libguile/print.c (iprin1): Likewise.

* libguile/gsubr.c (create_gsubr): Use `unsigned int's for REQ, OPT and
  RST.  Use `scm_tc7_gsubr' instead of `scm_makcclo ()' in the default
  case.
  (scm_gsubr_apply): Remove calls to `SCM_GSUBR_PROC ()'.
  (scm_f_gsubr_apply): Remove.

* libguile/gsubr.h (SCM_GSUBR_TYPE): New definition.
  (SCM_GSUBR_MAX): Changed to 33.
  (SCM_SET_GSUBR_TYPE, SCM_GSUBR_PROC, SCM_SET_GSUBR_PROC,
  scm_f_gsubr_apply): Remove.

* libguile/procprop.c (scm_i_procedure_arity): Remove reference to
  `scm_tc7_cclo'; add proper handling of `scm_tc7_gsubr'.

* libguile/procs.c (scm_makcclo, scm_make_cclo): Remove.
  (scm_procedure_p): Remove reference to `scm_tc7_cclo'.
  (scm_thunk_p): Likewise, plus add proper `scm_tc7_gsubr' handling.

* libguile/procs.h (SCM_CCLO_LENGTH, SCM_MAKE_CCLO_TAG,
  SCM_SET_CCLO_LENGTH, SCM_CCLO_BASE, SCM_SET_CCLO_BASE, SCM_CCLO_REF,
  SCM_CCLO_SET, SCM_CCLO_SUBR, SCM_SET_CCLO_SUBR, scm_makcclo,
  scm_make_cclo): Remove.

libguile/eval.i.c
@@ -1,7 +1,7 @@
 /*
  * eval.i.c - actual evaluator code for GUILE
  *
- * Copyright (C) 2002, 03, 04, 05, 06, 07 Free Software Foundation, Inc.
+ * Copyright (C) 2002, 03, 04, 05, 06, 07, 09 Free Software Foundation, Inc.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -1124,14 +1124,12 @@ dispatch:
 	if (!SCM_SMOB_APPLICABLE_P (proc))
 	  goto badfun;
 	RETURN (SCM_SMOB_APPLY_0 (proc));
-      case scm_tc7_cclo:
-	arg1 = proc;
-	proc = SCM_CCLO_SUBR (proc);
+      case scm_tc7_gsubr:
 #ifdef DEVAL
 	debug.info->a.proc = proc;
-	debug.info->a.args = scm_list_1 (arg1);
+	debug.info->a.args = SCM_EOL;
 #endif
-	goto evap1;
+	RETURN (scm_gsubr_apply (scm_list_1 (proc)));
       case scm_tc7_pws:
 	proc = SCM_PROCEDURE (proc);
 #ifdef DEVAL
@@ -1245,15 +1243,12 @@ dispatch:
 	    if (!SCM_SMOB_APPLICABLE_P (proc))
 	      goto badfun;
 	    RETURN (SCM_SMOB_APPLY_1 (proc, arg1));
-	  case scm_tc7_cclo:
-	    arg2 = arg1;
-	    arg1 = proc;
-	    proc = SCM_CCLO_SUBR (proc);
+	  case scm_tc7_gsubr:
 #ifdef DEVAL
 	    debug.info->a.args = scm_cons (arg1, debug.info->a.args);
 	    debug.info->a.proc = proc;
 #endif
-	    goto evap2;
+	    RETURN (scm_gsubr_apply (scm_list_2 (proc, arg1)));
 	  case scm_tc7_pws:
 	    proc = SCM_PROCEDURE (proc);
 #ifdef DEVAL
@@ -1351,19 +1346,14 @@ dispatch:
 	      goto badfun;
 	    RETURN (SCM_SMOB_APPLY_2 (proc, arg1, arg2));
 	  cclon:
-	  case scm_tc7_cclo:
+	  case scm_tc7_gsubr:
 #ifdef DEVAL
-	    RETURN (SCM_APPLY (SCM_CCLO_SUBR (proc),
-			       scm_cons (proc, debug.info->a.args),
-			       SCM_EOL));
+	    RETURN (scm_gsubr_apply (scm_cons (proc, debug.info->a.args)));
 #else
-	    RETURN (SCM_APPLY (SCM_CCLO_SUBR (proc),
-			       scm_cons2 (proc, arg1,
-					  scm_cons (arg2,
-						    scm_ceval_args (x,
-								   env,
-								   proc))),
-			       SCM_EOL));
+	    RETURN (scm_gsubr_apply
+		     (scm_cons (proc,
+				scm_cons2 (arg1, arg2,
+					   scm_ceval_args (x, env, proc)))));
 #endif
 	  case scm_tcs_struct:
 	    if (SCM_OBJ_CLASS_FLAGS (proc) & SCM_CLASSF_PURE_GENERIC)
@@ -1492,7 +1482,7 @@ dispatch:
 	    goto badfun;
 	  RETURN (SCM_SMOB_APPLY_3 (proc, arg1, arg2,
 				    SCM_CDDR (debug.info->a.args)));
-	case scm_tc7_cclo:
+	case scm_tc7_gsubr:
 	  goto cclon;
 	case scm_tc7_pws:
 	  proc = SCM_PROCEDURE (proc);
@@ -1555,7 +1545,7 @@ dispatch:
 	    goto badfun;
 	  RETURN (SCM_SMOB_APPLY_3 (proc, arg1, arg2,
 				    scm_ceval_args (x, env, proc)));
-	case scm_tc7_cclo:
+	case scm_tc7_gsubr:
 	  goto cclon;
 	case scm_tc7_pws:
 	  proc = SCM_PROCEDURE (proc);
@@ -1867,19 +1857,15 @@ tail:
 	RETURN (SCM_SMOB_APPLY_2 (proc, arg1, SCM_CAR (args)));
       else
 	RETURN (SCM_SMOB_APPLY_3 (proc, arg1, SCM_CAR (args), SCM_CDR (args)));
-    case scm_tc7_cclo:
+    case scm_tc7_gsubr:
 #ifdef DEVAL
       args = (SCM_UNBNDP(arg1) ? SCM_EOL : debug.vect[0].a.args);
-      arg1 = proc;
-      proc = SCM_CCLO_SUBR (proc);
       debug.vect[0].a.proc = proc;
       debug.vect[0].a.args = scm_cons (arg1, args);
 #else
       args = (SCM_UNBNDP(arg1) ? SCM_EOL : scm_cons (arg1, args));
-      arg1 = proc;
-      proc = SCM_CCLO_SUBR (proc);
 #endif
-      goto tail;
+      RETURN (scm_gsubr_apply (scm_cons (proc, args)));
     case scm_tc7_pws:
       proc = SCM_PROCEDURE (proc);
 #ifdef DEVAL

libguile/__scm.h
@@ -3,7 +3,7 @@
 #ifndef SCM___SCM_H
 #define SCM___SCM_H
 
-/* Copyright (C) 1995,1996,1998,1999,2000,2001,2002,2003, 2006, 2007, 2008 Free Software Foundation, Inc.
+/* Copyright (C) 1995,1996,1998,1999,2000,2001,2002,2003, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -139,8 +139,6 @@
  * These must be defined as given here.
  */
 
-
-#define CCLO
 
 /* Guile Scheme supports the #f/() distinction; Guile Lisp won't.  We
    have horrible plans for their unification.  */

libguile/debug.c
@@ -1,5 +1,5 @@
 /* Debugging extensions for Guile
- * Copyright (C) 1995,1996,1997,1998,1999,2000,2001, 2002, 2003, 2006, 2008 Free Software Foundation
+ * Copyright (C) 1995,1996,1997,1998,1999,2000,2001, 2002, 2003, 2006, 2008, 2009 Free Software Foundation
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -352,9 +352,6 @@ SCM_DEFINE (scm_procedure_source, "procedure-source", 1, 0, 0,
     if (!SCM_SMOB_DESCRIPTOR (proc).apply)
       break;
   case scm_tcs_subrs:
-#ifdef CCLO
-  case scm_tc7_cclo:
-#endif
   procprop:
     /* It would indeed be a nice thing if we supplied source even for
        built in procedures! */
@@ -385,9 +382,6 @@ SCM_DEFINE (scm_procedure_environment, "procedure-environment", 1, 0, 0,
   case scm_tcs_closures:
     return SCM_ENV (proc);
   case scm_tcs_subrs:
-#ifdef CCLO
-  case scm_tc7_cclo:
-#endif
     return SCM_EOL;
   default:
     SCM_WRONG_TYPE_ARG (1, proc);

libguile/eval.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008
+/* Copyright (C) 1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009
  * Free Software Foundation, Inc.
  * 
  * This library is free software; you can redistribute it and/or
@@ -3243,7 +3243,7 @@ scm_trampoline_0 (SCM proc)
       break;
     case scm_tc7_asubr:
     case scm_tc7_rpsubr:
-    case scm_tc7_cclo:
+    case scm_tc7_gsubr:
     case scm_tc7_pws:
       trampoline = scm_call_0;
       break;
@@ -3369,7 +3369,7 @@ scm_trampoline_1 (SCM proc)
       break;
     case scm_tc7_asubr:
     case scm_tc7_rpsubr:
-    case scm_tc7_cclo:
+    case scm_tc7_gsubr:
     case scm_tc7_pws:
       trampoline = scm_call_1;
       break;
@@ -3463,7 +3463,7 @@ scm_trampoline_2 (SCM proc)
       else
 	return NULL;
       break;
-    case scm_tc7_cclo:
+    case scm_tc7_gsubr:
     case scm_tc7_pws:
       trampoline = scm_call_2;
       break;

libguile/evalext.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1998,1999,2000,2001,2002,2003, 2006, 2008 Free Software Foundation, Inc.
+/* Copyright (C) 1998,1999,2000,2001,2002,2003, 2006, 2008, 2009 Free Software Foundation, Inc.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -106,7 +106,6 @@ SCM_DEFINE (scm_self_evaluating_p, "self-evaluating?", 1, 0, 0,
 	case scm_tc7_number:
 	case scm_tc7_string:
 	case scm_tc7_smob:
-	case scm_tc7_cclo:
 	case scm_tc7_pws:
 	case scm_tcs_subrs:
 	case scm_tcs_struct:

libguile/gc-card.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1995,1996,1997,1998,1999,2000,2001, 2002, 2004, 2005, 2006, 2007, 2008 Free Software Foundation, Inc.
+/* Copyright (C) 1995,1996,1997,1998,1999,2000,2001, 2002, 2004, 2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -130,14 +130,6 @@ scm_i_sweep_card (scm_t_cell *card, SCM *free_list, scm_t_heap_segment *seg)
 	case scm_tc7_vector:
 	  scm_i_vector_free (scmptr);
 	  break;
-
-#ifdef CCLO
-	case scm_tc7_cclo:
-	  scm_gc_free (SCM_CCLO_BASE (scmptr), 
-		       SCM_CCLO_LENGTH (scmptr) * sizeof (SCM),
-		       "compiled closure");
-	  break;
-#endif
 
 	case scm_tc7_number:
 	  switch SCM_TYP16 (scmptr)
@@ -397,10 +389,6 @@ scm_i_tag_name (scm_t_bits tag)
       return "weak vector";
     case scm_tc7_vector:
       return "vector";
-#ifdef CCLO
-    case scm_tc7_cclo:
-      return "compiled closure";
-#endif
     case scm_tc7_number:
       switch (tag)
 	{

libguile/gc-mark.c
@@ -294,21 +294,6 @@ scm_gc_mark_dependencies (SCM p)
 	}
       ptr = SCM_SIMPLE_VECTOR_REF (ptr, 0);
       goto gc_mark_loop;
-#ifdef CCLO
-    case scm_tc7_cclo:
-      {
-	size_t i = SCM_CCLO_LENGTH (ptr);
-	size_t j;
-	for (j = 1; j != i; ++j)
-	  {
-	    SCM obj = SCM_CCLO_REF (ptr, j);
-	    if (!SCM_IMP (obj))
-	      scm_gc_mark (obj);
-	  }
-	ptr = SCM_CCLO_REF (ptr, 0);
-	goto gc_mark_loop;
-      }
-#endif
 
     case scm_tc7_string:
       ptr = scm_i_string_mark (ptr);

libguile/goops.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1998,1999,2000,2001,2002,2003,2004,2008
+/* Copyright (C) 1998,1999,2000,2001,2002,2003,2004,2008,2009
  * Free Software Foundation, Inc.
  *
  * This library is free software; you can redistribute it and/or
@@ -233,7 +233,7 @@ SCM_DEFINE (scm_class_of, "class-of", 1, 0, 0,
 	    return scm_class_primitive_generic;
 	  else
 	    return scm_class_procedure;
-	case scm_tc7_cclo:
+	case scm_tc7_gsubr:
 	  return scm_class_procedure;
 	case scm_tc7_pws:
 	  return scm_class_procedure_with_setter;

commit 5b2f2c75527c5899648eec46c7eca0f57420ff3b

Fix the argument list of gsubr stack frames.
This fixes a bug introduced in e20d7001c3f7150400169fecb0bf0eefdf122fe2
and reported by Neil.

* libguile/eval.i.c (CEVAL)[DEVAL]: Don't duplicate ARG1 in
  `debug.info->a.args' for gsubr stack frames.
  (scm_apply): Likewise.

* test-suite/tests/eval.test ("stacks")["arguments of a gsubr stack
  frame"]: New test.

libguile/eval.i.c
@@ -1259,7 +1259,7 @@ dispatch:
 	    RETURN (SCM_SMOB_APPLY_1 (proc, arg1));
 	  case scm_tc7_gsubr:
 #ifdef DEVAL
-	    debug.info->a.args = scm_cons (arg1, debug.info->a.args);
+	    debug.info->a.args = debug.info->a.args;
 	    debug.info->a.proc = proc;
 #endif
 	    RETURN (scm_i_gsubr_apply (proc, arg1, SCM_UNDEFINED));
@@ -1896,7 +1896,7 @@ tail:
 #ifdef DEVAL
       args = (SCM_UNBNDP(arg1) ? SCM_EOL : debug.vect[0].a.args);
       debug.vect[0].a.proc = proc;
-      debug.vect[0].a.args = scm_cons (arg1, args);
+      debug.vect[0].a.args = args;
 #else
       args = (SCM_UNBNDP(arg1) ? SCM_EOL : scm_cons (arg1, args));
 #endif

test-suite/tests/eval.test
@@ -382,7 +382,30 @@
                                             hashq-ref)))
                                 frames)))))))
         (lambda (key result)
-          (= 1 result))))))
+          (= 1 result))))
+
+    (pass-if "arguments of a gsubr stack frame"
+      ;; Create a stack with two gsubr frames and make sure the arguments are
+      ;; correct.
+      (catch 'result
+        (lambda ()
+          (start-stack 'foo
+            (lazy-catch 'wrong-type-arg
+              (lambda ()
+                ;; Trigger a `wrong-type-arg' exception.
+                (substring 'wrong 'type 'arg))
+              (lambda _
+                (let* ((stack  (make-stack #t))
+                       (frames (stack->frames stack)))
+                  (throw 'result
+                         (map (lambda (frame)
+                                (cons (frame-procedure frame)
+                                      (frame-arguments frame)))
+                              frames)))))))
+        (lambda (key result)
+          (and (equal? (car result) `(,make-stack #t))
+               (pair? (member `(,substring  wrong type arg)
+                              (cdr result)))))))))
 
 ;;;
 ;;; letrec init evaluation