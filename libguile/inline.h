6d7c440288a65b9eccc65d2b27261c0b3625d1e9
Andy Wingo <wingo@pobox.com>
2009-10-15 15:44:45 +0200

    fix scm_array_handle_ref signedness fix
    * libguile/inline.h: Fix signedness fix.

--- libguile/inline.h
+++ libguile/inline.h
@@ -243,7 +243,7 @@ SCM_C_EXTERN_INLINE
 SCM
 scm_array_handle_ref (scm_t_array_handle *h, ssize_t p)
 {
-  if (SCM_UNLIKELY (p < 0 && -p > (ssize_t) h->base))
+  if (SCM_UNLIKELY (p < 0 && ((size_t)-p) > h->base))
     /* catch overflow */
     scm_out_of_range (NULL, scm_from_ssize_t (p));
   /* perhaps should catch overflow here too */
