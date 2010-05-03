42a710680d23d44fe43ae08339691e9424be4471
Andy Wingo <wingo@pobox.com>
2010-05-03 19:09:35 +0200

    add rnrs libraries test suite
    * test-suite/Makefile.am:
    * test-suite/tests/rnrs-libraries.test:
    * test-suite/tests/rnrs-test-a.scm: Add rnrs libraries test suite.

---
+++ test-suite/tests/rnrs-test-a.scm
@@ -0,0 +1,25 @@
+;;; test of defining rnrs libraries
+
+;;      Copyright (C) 2010 Free Software Foundation, Inc.
+;;
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 3 of the License, or (at your option) any later version.
+;; 
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;; 
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with this library; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+
+(library (test-suite tests rnrs-test-a)
+  (export double)
+  (import (guile))
+ 
+  (define (double x)
+    (* x 2)))
