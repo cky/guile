adb8f30600cdb146d6dd10684d9d9c6bcdd2cb76
Ludovic Courtès <ludo@gnu.org>
2010-05-24 23:11:13 +0200

    Add copyight/license header in `compile-psyntax.scm'.

--- module/ice-9/compile-psyntax.scm
+++ module/ice-9/compile-psyntax.scm
@@ -1,4 +1,24 @@
-(use-modules (language tree-il) (ice-9 pretty-print))
+;;; -*- mode: scheme; coding: utf-8; -*-
+;;;
+;;; Copyright (C) 2009, 2010 Free Software Foundation, Inc.
+;;;
+;;; This library is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU Lesser General Public
+;;; License as published by the Free Software Foundation; either
+;;; version 3 of the License, or (at your option) any later version.
+;;;
+;;; This library is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Lesser General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public
+;;; License along with this library; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+(use-modules (language tree-il)
+             (ice-9 pretty-print))
+
 (let ((source (list-ref (command-line) 1))
       (target (list-ref (command-line) 2)))
   (let ((in (open-input-file source))
