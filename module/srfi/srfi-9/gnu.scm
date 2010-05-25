e525e4e4995ea9115bd91e0fd6ce25b1bcdb6790
No Itisnt <theseaisinhere+git@gmail.com>
2010-05-25 15:47:35 -0500

    add custom record printers
    * doc/ref/srfi-modules.texi: update documentation
    * module/srfi/srfi-9/gnu.scm: add `define-record-printer'

---
+++ module/srfi/srfi-9/gnu.scm
@@ -0,0 +1,31 @@
+;;; Extensions to SRFI-9
+
+;; 	Copyright (C) 2010 Free Software Foundation, Inc.
+;;
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 3 of the License, or (at your option) any later version.
+;; 
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;; 
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with this library; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+;;; Commentary:
+
+;; Extensions to SRFI-9. Fully documented in the Guile Reference Manual.
+
+;;; Code:
+
+(define-module (srfi srfi-9 gnu)
+  #:export (define-record-printer))
+
+(define-syntax define-record-printer
+  (syntax-rules ()
+    ((_ type thunk)
+     (struct-set! type vtable-index-printer thunk))))
