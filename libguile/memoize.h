698514350a424d74bc8654e75283c57cc4996726
Andy Wingo <wingo@pobox.com>
2010-05-27 11:34:48 +0200

    remove scm_memoizer_p and scm_memoizer declarations
    * libguile/memoize.h (scm_memoizer_p, scm_memoizer): Remove these
      declarations.

--- libguile/memoize.h
+++ libguile/memoize.h
@@ -104,9 +104,6 @@ SCM_INTERNAL SCM scm_memoized_typecode (SCM sym);
 SCM_INTERNAL SCM scm_memoize_variable_access_x (SCM memoized, SCM module);
 SCM_API SCM scm_memoized_p (SCM obj);
 
-SCM_INTERNAL SCM scm_memoizer_p (SCM obj);
-SCM_INTERNAL SCM scm_memoizer (SCM obj);
-
 SCM_INTERNAL void scm_init_memoize (void);
 
 
