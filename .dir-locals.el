720a91c131d0f4bb5300a865130ca9a1c2afde2c
Andy Wingo <wingo@pobox.com>
2010-07-16 11:50:51 +0200

    lower fill-column in .dir-locals.el
    * .dir-locals.el: Fill-columns back down to 72.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,9 @@
 ;; Per-directory local variables for GNU Emacs 23 and later.
 
-((nil             . ((fill-column . 80)
+((nil             . ((fill-column . 72)
                      (tab-width   .  8)))
  (c-mode          . ((c-file-style . "gnu")))
  (scheme-mode     . ((indent-tabs-mode . nil)))
  (emacs-lisp-mode . ((indent-tabs-mode . nil)))
- (texinfo-mode    . ((indent-tabs-mode . nil))))
+ (texinfo-mode    . ((indent-tabs-mode . nil)
+                     (fill-column . 72))))
