8175a07e34605cf3e716eab380b195cf8f3b5f2b
Andreas Rottmann <a.rottmann@gmx.at>
2010-10-03 12:14:21 +0200

    Add implementation of SRFI-67
    * module/srfi/srfi-67/compare.scm: New file; reference implementation of
      SRFI 67.
    * module/srfi/srfi-67.scm: New module; includes the refernce
      implementation.
    * module/Makefile.am (SRFI_SOURCES): Add srfi/srfi-67.scm.
      (NOCOMP_SOURCES): Add srfi/srfi-67/compare.scm.
    
    * test-suite/tests/srfi-67.test: New file.
    * test-suite/Makefile.am (SCM_TESTS): Add tests/srfi-67.test.

---
+++ module/srfi/srfi-67.scm
@@ -0,0 +1,87 @@
+;;; srfi-67.scm --- Compare Procedures
+
+;; Copyright (C) 2010 Free Software Foundation, Inc.
+
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 3 of the License, or (at your option) any later version.
+
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with this library. If not, see
+;; <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This module is not yet documented in the Guile Reference Manual.
+
+;;; Code:
+
+(define-module (srfi srfi-67)
+  #:export (</<=?
+            </<?
+            <=/<=?
+            <=/<?
+            <=?
+            <?
+            =?
+            >/>=?
+            >/>?
+            >=/>=?
+            >=/>?
+            >=?
+            >?
+            boolean-compare
+            chain<=?
+            chain<?
+            chain=?
+            chain>=?
+            chain>?
+            char-compare
+            char-compare-ci
+            compare-by<
+            compare-by<=
+            compare-by=/<
+            compare-by=/>
+            compare-by>
+            compare-by>=
+            complex-compare
+            cond-compare
+            debug-compare
+            default-compare
+            if-not=?
+            if3
+            if<=?
+            if<?
+            if=?
+            if>=?
+            if>?
+            integer-compare
+            kth-largest
+            list-compare
+            list-compare-as-vector
+            max-compare
+            min-compare
+            not=?
+            number-compare
+            pair-compare
+            pair-compare-car
+            pair-compare-cdr
+            pairwise-not=?
+            rational-compare
+            real-compare
+            refine-compare
+            select-compare
+            string-compare
+            string-compare-ci
+            symbol-compare
+            vector-compare
+            vector-compare-as-list)
+  #:use-module (srfi srfi-27))
+
+(include-from-path "srfi/srfi-67/compare.scm")
