667230810b2ef95b6e8ee0b33ff1cdb61cc3ca21
Ludovic Courtès <ludo@gnu.org>
2011-01-24 23:37:46 +0100

    Install the current locale when starting the REPL.
    * module/ice-9/top-repl.scm (top-repl): Install the current locale.

--- module/ice-9/top-repl.scm
+++ module/ice-9/top-repl.scm
@@ -1,7 +1,7 @@
 ;;; -*- mode: scheme; coding: utf-8; -*-
 
-;;;; Copyright (C) 1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010
-;;;; Free Software Foundation, Inc.
+;;;; Copyright (C) 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
+;;;;   2005, 2006, 2007, 2008, 2009, 2010, 2011 Free Software Foundation, Inc.
 ;;;;
 ;;;; This library is free software; you can redistribute it and/or
 ;;;; modify it under the terms of the GNU Lesser General Public
@@ -64,6 +64,8 @@
 
     (call-with-sigint
      (lambda ()
+       (and (defined? 'setlocale)
+            (setlocale LC_ALL ""))
        (let ((status (start-repl 'scheme)))
          (run-hook exit-hook)
          status)))))
