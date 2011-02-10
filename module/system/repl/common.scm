1a3152f7df494e7469f5d12ee9a9a10356c56004
Mark H Weaver <mhw@netris.org>
2011-02-10 14:12:12 -0500

    Bump copyright date in REPL version string
    * module/system/repl/common.scm (*version*): Add 2011 to copyright
      date range.

--- module/system/repl/common.scm
+++ module/system/repl/common.scm
@@ -36,7 +36,7 @@
 
 (define *version*
   (format #f "GNU Guile ~A
-Copyright (C) 1995-2010 Free Software Foundation, Inc.
+Copyright (C) 1995-2011 Free Software Foundation, Inc.
 
 Guile comes with ABSOLUTELY NO WARRANTY; for details type `,show w'.
 This program is free software, and you are welcome to redistribute it
