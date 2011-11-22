7f893030abaf49711f948d76f124e18bfa9e4181
Ludovic Courtès <ludo@gnu.org>
2011-11-22 12:11:48 +0100

    Install the current locale if GUILE_INSTALL_LOCALE is nonzero.
    * libguile/guile.c (get_integer_from_environment)
      (should_install_locale): New functions.
      (main): Add `setlocale' call.
    
    * doc/ref/guile-invoke.texi (Environment Variables): Add documentation.

--- libguile/guile.c
+++ libguile/guile.c
@@ -1,5 +1,6 @@
-/* Copyright (C) 1996,1997,2000,2001, 2006, 2008, 2011 Free Software Foundation, Inc.
- * 
+/* Copyright (C) 1996, 1997, 2000, 2001, 2006, 2008,
+ *   2011, 2013 Free Software Foundation, Inc.
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 3 of
@@ -36,6 +37,7 @@
 #include <libguile/scmconfig.h>
 #endif
 #include <ltdl.h>
+#include <locale.h>
 
 #ifdef HAVE_WINSOCK2_H
 #include <winsock2.h>
@@ -64,9 +66,44 @@ inner_main (void *closure SCM_UNUSED, int argc, char **argv)
 #endif /* __MINGW32__ */
 }
 
+static int
+get_integer_from_environment (const char *var, int def)
+{
+  char *end = 0;
+  char *val = getenv (var);
+  long res = def;
+  if (!val)
+    return def;
+  res = strtol (val, &end, 10);
+  if (end == val)
+    {
+      fprintf (stderr, "guile: warning: invalid %s: %s\n", var, val);
+      return def;
+    }
+  return res;
+}
+
+static int
+should_install_locale (void)
+{
+  /* If the GUILE_INSTALL_LOCALE environment variable is set to a
+     nonzero value, we should install the locale via setlocale().  This
+     behavior is off by default for compatibility with previous 2.0.x
+     releases.  It will be on by default in 2.2.  */
+  return get_integer_from_environment ("GUILE_INSTALL_LOCALE", 0);
+}
+
 int
 main (int argc, char **argv)
 {
+  /* If we should install a locale, do it right at the beginning so that
+     string conversion for command-line arguments, along with possible
+     error messages, use the right locale.  See
+     <https://lists.gnu.org/archive/html/guile-devel/2011-11/msg00041.html>
+     for the rationale.  */
+  if (should_install_locale () && setlocale (LC_ALL, "") == NULL)
+    fprintf (stderr, "guile: warning: failed to install locale\n");
+
   scm_install_gmp_memory_functions = 1;
   scm_boot_guile (argc, argv, inner_main, 0);
   return 0; /* never reached */
