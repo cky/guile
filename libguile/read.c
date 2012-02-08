58996e37bba53ae91e6ecff56aa2bb155047bc1e
Mark H Weaver <mhw@netris.org>
2012-02-08 03:14:17 -0500

    Remove incorrect comment in read.c
    * libguile/read.c (scm_read_sharp): Remove incorrect comment that
      claims that scm_read_boolean might return a SRFI-4 vector.

--- libguile/read.c
+++ libguile/read.c
@@ -1,5 +1,5 @@
-/* Copyright (C) 1995,1996,1997,1999,2000,2001,2003, 2004, 2006, 2007, 2008, 2009, 2010, 2011 Free Software
- * Foundation, Inc.
+/* Copyright (C) 1995, 1996, 1997, 1999, 2000, 2001, 2003, 2004, 2006,
+ *   2007, 2008, 2009, 2010, 2011, 2012 Free Software Foundation, Inc.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -1332,7 +1332,6 @@ scm_read_sharp (scm_t_wchar chr, SCM port)
     case 't':
     case 'T':
     case 'F':
-      /* This one may return either a boolean or an SRFI-4 vector.  */
       return (scm_read_boolean (chr, port));
     case ':':
       return (scm_read_keyword (chr, port));
