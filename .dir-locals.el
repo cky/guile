e8772a9edef3fed3c592e61bbde618de197f4b53
Ludovic Courtès <ludo@gnu.org>
2012-11-24 00:22:20 +0100

    Augment `.dir-locals.el'.
    * .dir-locals.el: Add more `scheme-indent-function' rules.

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,7 +5,10 @@
  (c-mode          . ((c-file-style . "gnu")))
  (scheme-mode
   . ((indent-tabs-mode . nil)
-     (eval . (put 'pass-if-equal 'scheme-indent-function 2))))
+     (eval . (put 'pass-if 'scheme-indent-function 1))
+     (eval . (put 'pass-if-exception 'scheme-indent-function 2))
+     (eval . (put 'pass-if-equal 'scheme-indent-function 2))
+     (eval . (put 'with-test-prefix 'scheme-indent-function 1))))
  (emacs-lisp-mode . ((indent-tabs-mode . nil)))
  (texinfo-mode    . ((indent-tabs-mode . nil)
                      (fill-column . 72))))
