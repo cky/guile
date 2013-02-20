f5ea559aa04186534ea6771e3c15d5201fe85c67
Andy Wingo <wingo@pobox.com>
2013-02-20 22:57:02 +0100

    net_db.c doesn't import winsock2.h
    * libguile/net_db.c: Don't include winsock2.h; gnulib handles this for
      us as necessary.

--- libguile/net_db.c
+++ libguile/net_db.c
@@ -1,6 +1,6 @@
 /* "net_db.c" network database support
  * Copyright (C) 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2006, 2009,
- *   2010, 2011, 2012 Free Software Foundation, Inc.
+ *   2010, 2011, 2012, 2013 Free Software Foundation, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -50,14 +50,10 @@
 
 #include <sys/types.h>
 
-#ifdef HAVE_WINSOCK2_H
-#include <winsock2.h>
-#else
 #include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#endif
 
 #ifdef __MINGW32__
 #include "win32-socket.h"
