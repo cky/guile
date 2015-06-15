ed0c14769acda3134fde3737a3c17e68b49f6259
Natanael Copa <ncopa@alpinelinux.org>
2015-06-15 10:05:41 +0200

    i18n: Check for non-POSIX strtol_l.
    * configure.ac: Check for strtol_l.
    * libguile/i18n.c: Check HAVE_STRTOL_L before using strtol_l.

--- libguile/i18n.c
+++ libguile/i18n.c
@@ -1373,7 +1373,7 @@ SCM_DEFINE (scm_locale_string_to_integer, "locale-string->integer",
 
   if (c_locale != NULL)
     {
-#ifdef USE_GNU_LOCALE_API
+#if defined(USE_GNU_LOCALE_API) && defined(HAVE_STRTOL_L)
       c_result = strtol_l (c_str, &c_endptr, c_base, c_locale);
 #else
       RUN_IN_LOCALE_SECTION (c_locale,
